396ee91487361ac7440e4af39216d05166b29a6c
Bora Beşiktepe <borabesiktepe@gmail.com>
2023-03-17 16:27:00 +0300

    Updated CSS and Added script_workplace_panel.js

---
+++ src/main/resources/static/js/script_workplace_panel.js
@@ -0,0 +1,18 @@
+console.log("Selam");
+
+const workplaceSelect = document.querySelector("select");
+
+//API VERİ YÜKLEME
+fetch(`http://localhost:8080/api/workplaces/getworkplaces`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(workplace => {
+            const markupOption = `<option value="${workplace.name}">${workplace.name}</option>`;
+            workplaceSelect.insertAdjacentHTML('beforeend', markupOption);
+
+            console.log("Çalışma alanları listelendi.");
+        });
+    })
+    .catch(error => console.log(error));

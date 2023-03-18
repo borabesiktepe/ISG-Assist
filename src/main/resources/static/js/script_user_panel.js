5dcd2bb319ef25213d2614febac48a37828d5ca0
Bora Beşiktepe <borabesiktepe@gmail.com>
2023-03-18 20:28:18 +0300

    Updated User Panel

--- src/main/resources/static/js/script_user_panel.js
+++ src/main/resources/static/js/script_user_panel.js
@@ -1,13 +1,10 @@
 console.log("Selam");
 
 const workplaceList = document.querySelector(".workplace-management ul");
-const workplaceSelect = document.querySelector("select");
-const workplaceInput = document.querySelector(".workplace-controls input");
-const workplaceAddButton = document.querySelector(".add");
-const workplaceDeleteButton = document.querySelector(".remove");
 const noteArea = document.querySelector("textarea");
 
 //API VERİ YÜKLEME
+//ÇALIŞMA ALANLARI
 fetch(`http://localhost:8080/api/workplaces/getall`)
     .then(function (response) {
         return response.json();
@@ -15,16 +12,14 @@ fetch(`http://localhost:8080/api/workplaces/getall`)
     .then(data => {
         data.forEach(workplace => {
             const markupLi = `<li>${workplace.name}</li>`;
-            const markupOption = `<option value="${workplace.name}">${workplace.name}</option>`;
-
             workplaceList.insertAdjacentHTML('beforeend', markupLi);
-            workplaceSelect.insertAdjacentHTML('beforeend', markupOption);
 
             console.log("Çalışma alanları listelendi.");
         });
     })
     .catch(error => console.log(error));
 
+//NOTLAR
 fetch(`http://localhost:8080/api/notes/getall`)
     .then(function (response) {
         return response.json();
@@ -37,18 +32,3 @@ fetch(`http://localhost:8080/api/notes/getall`)
         });
     })
     .catch(error => console.log(error));
-
-//WORKPLACE BUTONLARI
-workplaceAddButton.addEventListener("click", () => {
-    if (workplaceInput.value === "") {
-        workplaceInput.placeholder = "Bir çalışma alanı seçin";
-        workplaceInput.style.borderColor = "red";
-    }
-})
-
-workplaceDeleteButton.addEventListener("click", () => {
-    if (workplaceInput.value === "") {
-        workplaceInput.placeholder = "Bir çalışma alanı seçin";
-        workplaceInput.style.borderColor = "red";
-    }
-})

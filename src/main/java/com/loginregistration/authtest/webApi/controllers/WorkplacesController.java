a3d82458f79f952d34e71c009a8a86f3e80301c8
Bora Beşiktepe <borabesiktepe@gmail.com>
2023-03-21 17:20:54 +0300

    Added Script file to Create Workplace Form

--- src/main/java/com/loginregistration/authtest/webApi/controllers/WorkplacesController.java
+++ src/main/java/com/loginregistration/authtest/webApi/controllers/WorkplacesController.java
@@ -6,10 +6,7 @@ import com.loginregistration.authtest.business.responses.WorkplacesResponse;
 import com.loginregistration.authtest.entities.User;
 import com.loginregistration.authtest.security.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,7 +35,7 @@ public class WorkplacesController {
 	}
 	
 	@PostMapping("/add")
-	public void add(CreateWorkplaceRequest createWorkplaceRequest) {
+	public void add(@RequestBody() CreateWorkplaceRequest createWorkplaceRequest) {
 		this.workplaceService.add(createWorkplaceRequest);
 	}
 }
---
+++ src/main/resources/static/js/script_createworkplace.js
@@ -0,0 +1,28 @@
+console.log("Selam. Burası Workplace Oluşturma Form Sayfası");
+
+const form = document.getElementById("form");
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var description = document.getElementById('description').value;
+
+    fetch('http://localhost:8080/api/workplaces/add', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: name,
+            body: description,
+
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+        }).catch(error => console.error('Error:', error));
+});

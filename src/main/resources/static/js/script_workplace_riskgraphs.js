89f10e8fd133e59d46925e7b8b2eb38f5f54255b
Bora Beşiktepe <borabesiktepe@gmail.com>
2023-04-08 16:15:07 +0300

    Updated Risk Graph JS

--- src/main/resources/static/js/script_workplace_riskgraphs.js
+++ src/main/resources/static/js/script_workplace_riskgraphs.js
@@ -31,7 +31,7 @@ fetch('https://64302442c26d69edc88bce05.mockapi.io/api/graph/enTekrarEdenTehlike
             options: {
                 title: {
                     display: true,
-                    text: "Tehlike Sayıları"
+                    text: "En çok tekrar eden tehlikeler"
                 }
             }
         });
@@ -71,14 +71,14 @@ fetch('https://64302442c26d69edc88bce05.mockapi.io/api/graph/enTehlikeliYerEkipm
             options: {
                 title: {
                     display: true,
-                    text: "Yer/Ekipmanlara Göre Tehlike Sayıları"
+                    text: "En Tehlikeli Yer/Ekipmanlar"
                 }
             }
         });
     })
 
-var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-var yValues = [55, 49, 44, 24, 15];
+var xValues = ["Italy", "France", "Spain", "USA", "Argentina", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K"];
+var yValues = [55, 49, 44, 24, 15, 55, 49, 44, 24, 15, 55, 49, 44, 24, 15, 55, 49, 44, 24, 15];
 let barColors = [
     "#FFC40C",
     "#96BD33",
@@ -88,8 +88,15 @@ let barColors = [
     "#2F4858"
 ];
 
+for (i = 0; i < xValues.length; i++) {
+    const R = Math.floor(Math.random() * 255);
+    const G = Math.floor(Math.random() * 255);
+    const B = Math.floor(Math.random() * 255);
+    barColors.push(`rgba(${R}, ${G}, ${B}, 1)`);
+}
+
 new Chart("chart1", {
-    type: "pie",
+    type: "doughnut",
     data: {
         labels: xValues,
         datasets: [{
